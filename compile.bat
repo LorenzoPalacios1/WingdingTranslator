3f9e0e374fec44614a5bd112ef44e2d6f079ef70
LorenzoPalacios1 <lp434717@gmail.com>
2024-08-13 13:19:34 -0500

    remove cloned/redundant files
    Signed-off-by: LorenzoPalacios1 <lp434717@gmail.com>

--- compile.bat
+++
@@ -1,1 +0,0 @@
-clang -fstack-protector-strong -Og -pedantic -Wall -Werror -Wextra -Wfloat-equal -Wformat=2 -Wimplicit-atomic-properties -Wmissing-declarations -Wmissing-prototypes -Woverlength-strings -Wshadow -D_FORTIFY_SOURCE=2 Translator.c "C-MyBasics\MyBasics.c" -o Translator
